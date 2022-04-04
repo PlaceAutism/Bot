0ab29b74e4e7b661b4f59a883407dd87550f6a06
Lenni Hein <git@lennihein.com>
2022-04-04 14:23:49 +0200

    changed source

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -11,8 +11,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @updateURL    https://github.com/MrKev312/Bot/raw/master/placenlbot.user.js
-// @downloadURL  https://github.com/MrKev312/Bot/raw/master/placenlbot.user.js
+// @updateURL    https://github.com/placeautism/Bot/raw/master/placenlbot.user.js
+// @downloadURL  https://github.com/placeautism/Bot/raw/master/placenlbot.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM.xmlHttpRequest
